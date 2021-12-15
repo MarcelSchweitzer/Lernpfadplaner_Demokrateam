fe98b18e49e44c8a1fd1756ca586f65357d9343c
maxmo <max.montag1994@gmail.com>
2021-12-15 22:09:22 +0100

    -m running server

--- app.js
+++ app.js
@@ -1,17 +1,44 @@
-console.log('hi');
-
 const express = require('express');
 const path = require('path');
+const session = require('./backend/src/js/session.cjs')
+
 const app = express();
 
-app.use('/public', express.static(path.join(__dirname, 'static')));
+// create user session
+var sess = new session.Session();
 
+// create test learning path
+sess.createLearningPath();
+
+sess.addLearningPath();
+sess.addLearningPath();
+sess.addLearningPath();
+sess.addLearningPath();
+sess.addLearningPath();
+sess.addLearningPath();
+
+console.log(sess.getLearningPathIds())
+
+console.log('current name ' + sess.readCurrentLearningPath().getName())
+console.log('current id ' + sess.readCurrentLearningPath().getId())
+sess.openLearningPath(sess.getLearningPathIds()[2])
+console.log('current id ' + sess.readCurrentLearningPath().getId())
+console.log('current name ' + sess.readCurrentLearningPath().getName())
+sess.removeLearningPath(sess.getCurrentLearningPathId())
+console.log(sess.getLearningPathIds())
+console.log('current id ' + sess.getCurrentLearningPathId())
+sess.createLearningPath();
+
+app.use('/public', express.static(path.join(__dirname, 'static')));
+app.set('view engine', 'ejs');
+
+// render index.ejs
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'static', 'index.html'));
+  res.render('index', {});
 })
 
 app.get('/editModule', function (req, res) {
-  res.send('Editing Screen')
+  res.render('learningPathEditor', {data : {currentModuleName: sess.readCurrentLearningPath().getName()}});
 })
 
-app.listen(8080)
+app.listen(8082)
--- backend/tests/arrayToolkit.test.js
+++ backend/tests/arrayToolkit.test.js
@@ -1,1 +1,33 @@
-// TODO JEST TEST
+// TODO TEST
+
+// TODO Rewrite to actual test (Jest)
+var test_arr = []
+
+test_arr = arrTK.insertAt(test_arr, "max")
+console.log(test_arr);
+test_arr = arrTK.insertAt(test_arr, "daniel", 0)
+console.log(test_arr);
+test_arr = arrTK.insertAt(test_arr, "adrian", 0)
+console.log(test_arr);
+test_arr = arrTK.insertAt(test_arr, "hannah", 2)
+console.log(test_arr);
+test_arr = arrTK.insertAt(test_arr, "wassim", 1)
+console.log(test_arr);
+test_arr = arrTK.insertAt(test_arr, "malcolm")
+console.log(test_arr);
+test_arr = arrTK.insertAt(test_arr, "hadis", 2)
+console.log(test_arr);
+test_arr = arrTK.insertAt(test_arr, "behnaz", 1)
+console.log(test_arr);
+test_arr = arrTK.rmByIndex(test_arr, 2)
+console.log(test_arr);
+test_arr = arrTK.mvByIndex(test_arr, 0, 1)
+console.log(test_arr);
+test_arr = arrTK.mvByIndex(test_arr, 4, 2)
+console.log(test_arr);
+test_arr = arrTK.mvByIndex(test_arr, 2, 5)
+console.log(test_arr);
+test_arr = arrTK.mvByIndex(test_arr, 5, 2)
+console.log(test_arr);
+test_arr = arrTK.mvByIndex(test_arr, 0, 4)
+console.log(test_arr);
