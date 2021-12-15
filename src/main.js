d1bb1e0263f5503b889807061ac8fdcde0c2eacf
maxmo <max.montag1994@gmail.com>
2021-12-15 01:30:13 +0100

    minor changes to toolkit

--- src/main.js
+++ src/main.js
@@ -1,14 +1,20 @@
 // import LearningPath from LearningPath
-import {rmByIndex, rmByValue, mvByIndex} from "/src/modules/array_toolkit.mjs"
+import {rmByIndex, rmByValue, mvByIndex} from '/src/modules/array_toolkit.mjs'
 
-// lp = new LearningPath("test")
+// lp = new LearningPath('test')
 
 
 // test array_toolkit
-var test_arr = ["max", "wassim", "hannah", "malcolm", "hadis", "behnaz", "daniel", "adrian"]
+var test_arr = ['max', 'wassim', 'hannah', 'malcolm', 'hadis', 'behnaz', 'daniel', 'adrian']
 
-test_arr = rmByValue(test_arr, "max")
+test_arr = rmByValue(test_arr, 'max')
 test_arr = rmByIndex(test_arr, 2)
 console.log(test_arr);
-test_arr = mvByIndex(test_arr, 2, 6)
+test_arr = mvByIndex(test_arr, 0, 5)
+console.log(test_arr);
+test_arr = mvByIndex(test_arr, 4, 2)
+console.log(test_arr);
+test_arr = mvByIndex(test_arr, 2, 5)
+console.log(test_arr);
+test_arr = mvByIndex(test_arr, 5, 2)
 console.log(test_arr);
--- src/modules/array_toolkit.mjs
+++ src/modules/array_toolkit.mjs
@@ -1,11 +1,11 @@
 // remove by index
 export function rmByIndex(arr, ind){
-  if(index >= arr.length || index < 0){
-    throw "Index Out of Bound!"
-  }
-  return arr.filter(function(value, index){ 
-    return index != ind;
-  });
+  if(ind >= arr.length || ind < 0)
+    throw 'Index Out of Bound!'
+  else
+    return arr.filter(function(value, index){ 
+      return index != ind;
+    });
 }
 
 // remove by value
@@ -18,20 +18,34 @@ export function rmByValue(arr, val){
 // move one elem from one index to another
 export function mvByIndex(arr, index, newIndex) {
   if(index >= arr.length || newIndex >= arr.length
-    || index < 0 || newIndex < 0){
-    throw "Index Out of Bound!"
+    || index < 0 || newIndex < 0 || arr.length < 1){
+    throw 'Index Out of Bound!'
   }else{
     if (newIndex != index) {
-      arr.splice(newIndex, 0, arr[index])
   
       // shift left
-      if (newIndex < index)
+      if (newIndex < index){
+        arr.splice(newIndex, 0, arr[index])
         arr = rmByIndex(arr, index + 1)
-  
+      }
+
       // shift right
-      else
+      else{
+        arr.splice(newIndex+1, 0, arr[index])
         arr = rmByIndex(arr, index)
+      }
     } 
   }
   return arr
+}
+
+export function insertAt(arr, element, index=null){
+  if(index > arr.length || index < 0)
+    throw 'Index Out of Bound!'
+
+  // insert at end of list if index was not passed
+  if(index === null)
+    index = arr.length
+  
+  return arr.splice(index, 0, element)  
 }
