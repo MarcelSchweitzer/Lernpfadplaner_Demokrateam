ab441e0e24f4451a1d9f654dc868085f0b2fd50e
maxmo <max.montag1994@gmail.com>
2021-12-16 00:19:49 +0100

    minor

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ installation:
 1) Navigate to https://nodejs.org/en/ and install the latest LTS version on your machine
 2) cd to main directory (containing app.js)
 3) run node app.js
-4) navigate to localhost/8080 with our browser
+4) navigate to localhost/8082 with our browser
--- app.js
+++ app.js
@@ -9,24 +9,14 @@ var sess = new session.Session();
 
 // create test learning path
 sess.createLearningPath();
-
 sess.addLearningPath();
 sess.addLearningPath();
 sess.addLearningPath();
 sess.addLearningPath();
 sess.addLearningPath();
 sess.addLearningPath();
-
-console.log(sess.getLearningPathIds())
-
-console.log('current name ' + sess.readCurrentLearningPath().getName())
-console.log('current id ' + sess.readCurrentLearningPath().getId())
 sess.openLearningPath(sess.getLearningPathIds()[2])
-console.log('current id ' + sess.readCurrentLearningPath().getId())
-console.log('current name ' + sess.readCurrentLearningPath().getName())
 sess.removeLearningPath(sess.getCurrentLearningPathId())
-console.log(sess.getLearningPathIds())
-console.log('current id ' + sess.getCurrentLearningPathId())
 sess.createLearningPath();
 
 app.use('/public', express.static(path.join(__dirname, 'static')));
@@ -34,11 +24,15 @@ app.set('view engine', 'ejs');
 
 // render index.ejs
 app.get('/', function (req, res) {
-  res.render('index', {});
+  res.render('index', {data: {
+    learningPathNames: sess.getLearningPathNames()
+  }});
 })
 
 app.get('/editModule', function (req, res) {
-  res.render('learningPathEditor', {data : {currentModuleName: sess.readCurrentLearningPath().getName()}});
+  res.render('learningPathEditor', {data: {
+    currentModuleName: sess.readCurrentLearningPath().getName()
+  }});
 })
 
 app.listen(8082)
