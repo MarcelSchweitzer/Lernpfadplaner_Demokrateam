b415b50f135d0008a529647cc966aba09b6009d5
maxmo <max.montag1994@gmail.com>
2022-01-24 18:23:29 +0100

    material url

--- public/js/eventHandler.js
+++ public/js/eventHandler.js
@@ -542,7 +542,7 @@ document.addEventListener("drop", (event) => {
             let interactionType = draggedInteraction.getAttribute('id').split('$$')[1];
             category.trim()
             interactionType.trim()
-            session.addInteraction(coordinates, "", "", category, interactionType,"");
+            session.addInteraction(coordinates, "", "", category, interactionType, " ", " ");
             unsavedChanges = true;
             session.openInteraction(session.getCurrentScenario().interactions.length - 1)
             refreshInteractivityList();
@@ -583,6 +583,7 @@ function refreshInteractivityInputs() {
 }
 
 function updateInteractionProperty(key, value) {
+    console.log(key+"  "+value)
     unsavedChanges = true;
     session.setInteractionProp(key, value);
 }
