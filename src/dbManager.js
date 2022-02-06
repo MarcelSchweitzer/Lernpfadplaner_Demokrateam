343d13e3a2ec463b5db13947e4340310365e7a97
jadeeule <hrploetz.organisation@gmail.com>
2022-02-06 14:23:14 +0100

    auto fixable problems fixed

--- src/dbManager.js
+++ src/dbManager.js
@@ -1,4 +1,4 @@
-const pgp = require('pg-promise')( /*options*/ );
+const pgp = require('pg-promise')(/* options */);
 const { Client } = require('pg');
 
 require('dotenv').config();
@@ -6,118 +6,113 @@ require('dotenv').config();
 function noop() {}
 
 const credentials = {
-    database: process.env.DB_Name,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-}
+  database: process.env.DB_Name,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+};
 
 // create a new db client (for non-blocking behaviour)
 function createClient() {
-    var dbClient = new Client(credentials);
-    dbClient.connect();
-    return dbClient;
+  const dbClient = new Client(credentials);
+  dbClient.connect();
+  return dbClient;
 }
 
 // select
 function select(from = '', select = '*', where = '', cb = noop) {
-    let dbClient = createClient();
-    let query = 'SELECT ' + select + ' FROM ' + from;
-    if (where != '')
-        query += ' WHERE ' + where;
-    // console.log(query);
-    dbClient.query(query, (err, res) => {
-        if (err) {
-            console.error(err);
-            return false
-        }
-        dbClient.end();
-        return cb(res.rows)
-    });
+  const dbClient = createClient();
+  let query = `SELECT ${select} FROM ${from}`;
+  if (where != '') { query += ` WHERE ${where}`; }
+  // console.log(query);
+  dbClient.query(query, (err, res) => {
+    if (err) {
+      console.error(err);
+      return false;
+    }
+    dbClient.end();
+    return cb(res.rows);
+  });
 }
 
 // select an exact match
 function selectMatch(from = '', select = '*', col, equals, cb = noop) {
-    let quotes = ((typeof equals != 'number') ? "'" : "");
-    let where = col + "=" + quotes + equals + quotes;
-    return module.exports.select(from, select, where, cb)
+  const quotes = ((typeof equals !== 'number') ? "'" : '');
+  const where = `${col}=${quotes}${equals}${quotes}`;
+  return module.exports.select(from, select, where, cb);
 }
 
 // insert into databas
 function insert(table, dict, cb = noop, errCb = noop) {
-    let dbClient = createClient();
-    let query = 'INSERT INTO ' + table + ' ('
-    for (let [key, value] of Object.entries(dict))
-        query += key + ','
-    query = query.slice(0, -1)
-    query += ') VALUES ('
-    for (let [key, value] of Object.entries(dict)) {
-        var quotes = ((typeof value != 'number') ? "'" : "");
-        query += quotes + value + quotes + ','
+  const dbClient = createClient();
+  let query = `INSERT INTO ${table} (`;
+  for (const [key, value] of Object.entries(dict)) { query += `${key},`; }
+  query = query.slice(0, -1);
+  query += ') VALUES (';
+  for (const [key, value] of Object.entries(dict)) {
+    const quotes = ((typeof value !== 'number') ? "'" : '');
+    query += `${quotes + value + quotes},`;
+  }
+  query = query.slice(0, -1);
+  query += ');';
+  // console.log(query)
+  dbClient.query(query, (err, res) => {
+    if (err) {
+      console.error(err);
+      return errCb();
     }
-    query = query.slice(0, -1)
-    query += ');'
-        // console.log(query)
-    dbClient.query(query, (err, res) => {
-        if (err) {
-            console.error(err);
-            return errCb()
-        }
-        dbClient.end()
-        return cb()
-    });
-
+    dbClient.end();
+    return cb();
+  });
 }
 
 // delete from Database
 function _delete(table, attrib, equals, cb = noop) {
-    let dbClient = createClient();
-    let query = 'DELETE FROM ' + table + ' WHERE ' + attrib + '=';
-    let quotes = ((typeof equals != 'number') ? "'" : "");
-    query += quotes + equals + quotes + ','
-    query = query.slice(0, -1)
-        //console.log(query)
-    dbClient.query(query, (err, res) => {
-        if (err) {
-            console.error(err);
-            return false
-        }
-        dbClient.end()
-        return cb()
-    });
+  const dbClient = createClient();
+  let query = `DELETE FROM ${table} WHERE ${attrib}=`;
+  const quotes = ((typeof equals !== 'number') ? "'" : '');
+  query += `${quotes + equals + quotes},`;
+  query = query.slice(0, -1);
+  // console.log(query)
+  dbClient.query(query, (err, res) => {
+    if (err) {
+      console.error(err);
+      return false;
+    }
+    dbClient.end();
+    return cb();
+  });
 }
 
 // update an entry
 function _update(table, key, equals, dict, cb = noop) {
-    let listOfKeys = Object.keys(dict);
-    let listOfValues = Object.values(dict)
+  const listOfKeys = Object.keys(dict);
+  const listOfValues = Object.values(dict);
 
-    if (listOfKeys.length != listOfValues.length)
-        return
+  if (listOfKeys.length != listOfValues.length) { return; }
 
-    let dbClient = createClient();
-    let query = 'UPDATE ' + table + ' SET '
+  const dbClient = createClient();
+  let query = `UPDATE ${table} SET `;
 
-    for (let i = 0; i < listOfKeys.length; i++) {
-        let quotes = ((typeof listOfValues[i] != 'number') ? "'" : "");
-        query += listOfKeys[i] + '=' + quotes + listOfValues[i] + quotes + ','
+  for (let i = 0; i < listOfKeys.length; i++) {
+    const quotes = ((typeof listOfValues[i] !== 'number') ? "'" : '');
+    query += `${listOfKeys[i]}=${quotes}${listOfValues[i]}${quotes},`;
+  }
+  query = query.slice(0, -1);
+  query += ` WHERE ${key}=`;
+  const _quotes = ((typeof equals !== 'number') ? "'" : '');
+  query += _quotes + equals + _quotes;
+
+  // console.log(query)
+  dbClient.query(query, (err, res) => {
+    if (err) {
+      console.error(err);
+      return false;
     }
-    query = query.slice(0, -1)
-    query += ' WHERE ' + key + '=';
-    let _quotes = ((typeof equals != 'number') ? "'" : "");
-    query += _quotes + equals + _quotes
-
-    //console.log(query)
-    dbClient.query(query, (err, res) => {
-        if (err) {
-            console.error(err);
-            return false
-        }
-        dbClient.end()
-        return cb()
-    });
+    dbClient.end();
+    return cb();
+  });
 }
-
 
 module.exports.select = select;
 module.exports.selectMatch = selectMatch;
--- src/uniqueIdentifiers.js
+++ src/uniqueIdentifiers.js
@@ -1,44 +1,41 @@
 const fstk = require('./fileSystemToolkit.js');
+
 const animalNameList = fstk.textToArray('./res/animalNames.txt');
 
 // return a unique id
 function uniqueId(listOfIds) {
-    if (listOfIds == null || listOfIds.length == 0)
-        return randomId = Math.floor(100000 + Math.random() * 9000000000);
-    while (true) {
-        let randomId = Math.floor(100000 + Math.random() * 9000000000);
-        if (!listOfIds.includes(randomId))
-            return randomId;
-    }
+  if (listOfIds == null || listOfIds.length == 0) { return randomId = Math.floor(100000 + Math.random() * 9000000000); }
+  while (true) {
+    const randomId = Math.floor(100000 + Math.random() * 9000000000);
+    if (!listOfIds.includes(randomId)) { return randomId; }
+  }
 }
 
 // return a standard name with incrementing numbers
 function uniqueName(schema, listOfNames) {
-    if (listOfNames == null || listOfNames.length == 0 || !listOfNames.includes(schema))
-        return schema;
-    let name = schema;
-    let num = 1;
+  if (listOfNames == null || listOfNames.length == 0 || !listOfNames.includes(schema)) { return schema; }
+  const name = schema;
+  let num = 1;
 
-    // search until free name has been found
-    while (true) {
-        let nextName = name + num.toString();
-        if (!listOfNames.includes(nextName))
-            return nextName;
-        num++;
-    }
+  // search until free name has been found
+  while (true) {
+    const nextName = name + num.toString();
+    if (!listOfNames.includes(nextName)) { return nextName; }
+    num++;
+  }
 }
 
 // create a username
 function createUserName() {
-    let randIndex = Math.floor(Math.random() * (animalNameList.length - 1));
-    let animal = animalNameList[randIndex];
-    animal = 'Anonyme ' + animal + ' ';
-    return animal
+  const randIndex = Math.floor(Math.random() * (animalNameList.length - 1));
+  let animal = animalNameList[randIndex];
+  animal = `Anonyme ${animal} `;
+  return animal;
 }
 
 // create a unique username
 function createUniqueUserName(listOfNames) {
-    return module.exports.uniqueName(module.exports.createUserName(), listOfNames);
+  return module.exports.uniqueName(module.exports.createUserName(), listOfNames);
 }
 
 module.exports.uniqueId = uniqueId;
